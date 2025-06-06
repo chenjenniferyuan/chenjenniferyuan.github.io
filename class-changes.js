9948333ab646960e260e3b9fb57bf7fc4fe7d5f7
Jennifer Chen <chenjenniferyuan@gmail.com>
2025-06-06 13:43:18 +1000

    timed svg colour change and stuff

--- class-changes.js
+++ class-changes.js
@@ -9,9 +9,18 @@ const classChanges = [
 
   { time: 63, target: "body", add: "dark-mode" },
 
-  
-  { time: 9, target: ".changeme", remove: "show" },
-  { time: 9, target: ".changeme", add: "hidden" },
+  { time: 28.75, target: ".sight", remove: "filter-yellow" },
+  { time: 29.5, target: ".sound", remove: "filter-yellow" },
+  { time: 30.5, target: ".smell", remove: "filter-yellow" },
+  { time: 31.5, target: ".taste", remove: "filter-yellow" },
+  { time: 32, target: ".touch", remove: "filter-yellow" },
+  { time: 32.5, target: ".air", remove: "filter-yellow" },
+  { time: 33, target: ".earth", remove: "filter-yellow" },
+  { time: 33.5, target: ".temperature", remove: "filter-yellow" },
+  { time: 34.5, target: ".agitation", remove: "filter-yellow" },
+  { time: 35.5, target: ".passion", remove: "filter-yellow" },
+  { time: 36, target: ".joy", remove: "filter-yellow" },
+  { time: 36.5, target: ".sadness", remove: "filter-yellow" },
 
   { time: 9, target: ".changeme2", add: "show" },
   { time: 16, target: ".changeme2", remove: "show" },
