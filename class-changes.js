ed06289fd799f7c10dcaf12aeede3908f0cd8a53
Jennifer Chen <chenjenniferyuan@gmail.com>
2025-06-08 16:44:40 +1000

    change humans humans things & horse no longer hover activate, added folio marks

--- class-changes.js
+++ class-changes.js
@@ -1,5 +1,5 @@
 const classChanges = [
-  { time: 0.2, target: ".titletext", add: "text-black" },
+  { time: 0.1, target: ".titletext", add: "text-black" },
   { time: 1.2, target: ".titlecaption", add: "show" },
   
   { time: 11, target: "div.border-black", add: "border-highlight" },
@@ -7,9 +7,17 @@ const classChanges = [
 
   { time: 18.5, target: ".friction", add: "text-yellow"},
 
-  { time: 63, target: "body", add: "dark-mode" },
+  { time: 22.8, target: ".humans1", add: "show" },
+  { time: 23.6, target: ".humans2", add: "show" },
+  { time: 24, target: ".things", add: "show" },
+  { time: 25.7, target: ".humans1", remove: "show" },
+  { time: 25.7, target: ".humans2", remove: "show" },
+  { time: 25.7, target: ".things", remove: "show" },
 
-  { time: 28.75, target: ".sight", remove: "filter-yellow" },
+  { time: 63, target: "body", add: "dark-mode" },
+  { time: 63, target: ".fullscreen-opaque", add: "dark-mode" },
+
+  { time: 29, target: ".sight", remove: "filter-yellow" },
   { time: 29.5, target: ".sound", remove: "filter-yellow" },
   { time: 30.5, target: ".smell", remove: "filter-yellow" },
   { time: 31.5, target: ".taste", remove: "filter-yellow" },
@@ -18,10 +26,13 @@ const classChanges = [
   { time: 33, target: ".earth", remove: "filter-yellow" },
   { time: 33.5, target: ".temperature", remove: "filter-yellow" },
   { time: 34.5, target: ".agitation", remove: "filter-yellow" },
-  { time: 35.5, target: ".passion", remove: "filter-yellow" },
+  { time: 35.2, target: ".passion", remove: "filter-yellow" },
   { time: 36, target: ".joy", remove: "filter-yellow" },
   { time: 36.5, target: ".sadness", remove: "filter-yellow" },
 
+
+
+  // idk
   { time: 9, target: ".changeme2", add: "show" },
   { time: 16, target: ".changeme2", remove: "show" },
   { time: 16, target: ".changeme2", add: "hidden" },
