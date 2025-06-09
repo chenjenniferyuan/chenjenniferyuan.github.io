fb8c2d37e3d0d03f32350b96e7160b0decc16bb8
Jennifer Chen <chenjenniferyuan@gmail.com>
2025-06-09 22:07:59 +1000

    mainly work on LOF section
    - fine tuned scroll elements
    - revised humans humans things/general pop up images classes
    - revised manifont underlay for 1. LOSS OF FRICTION
    - changed smiley font to courier ( will be cute ver on all browsers)
    - moved all body text in LOF from margin left 20% to margin left 3%
    - added pop up photos for all LOF paragraphs apart from last one
    - probably other stuff

--- class-changes.js
+++ class-changes.js
@@ -9,16 +9,13 @@ const classChanges = [
 
   { time: 22.8, target: ".humans1", add: "show" },
   { time: 23.6, target: ".humans2", add: "show" },
-  { time: 24, target: ".things", add: "show" },
+  { time: 24, target: ".things1", add: "show" },
   { time: 25.7, target: ".humans1", remove: "show" },
   { time: 25.7, target: ".humans2", remove: "show" },
-  { time: 25.7, target: ".things", remove: "show" },
+  { time: 25.7, target: ".things1", remove: "show" },
 
-  { time: 63, target: "body", add: "dark-mode" },
-  { time: 63, target: ".fullscreen-opaque", add: "dark-mode" },
-
-  { time: 29, target: ".sight", remove: "filter-yellow" },
-  { time: 29.5, target: ".sound", remove: "filter-yellow" },
+  { time: 28.9, target: ".sight", remove: "filter-yellow" },
+  { time: 29.6, target: ".sound", remove: "filter-yellow" },
   { time: 30.5, target: ".smell", remove: "filter-yellow" },
   { time: 31.5, target: ".taste", remove: "filter-yellow" },
   { time: 32, target: ".touch", remove: "filter-yellow" },
@@ -30,7 +27,39 @@ const classChanges = [
   { time: 36, target: ".joy", remove: "filter-yellow" },
   { time: 36.5, target: ".sadness", remove: "filter-yellow" },
 
+  { time: 63, target: "body", add: "dark-mode" },
+  { time: 63, target: ".fullscreen-opaque", add: "dark-mode" },
 
+  { time: 76.2, target: ".smiley", add: "show" },
+  { time: 77.5, target: ".functionandform", add: "text-yellow" },
+
+  { time: 88.8, target: ".things2", add: "show" },
+  { time: 90.1, target: ".devices", add: "show" },
+  { time: 91.2, target: ".things2", add: "border-dotted" },
+  { time: 94.7, target: ".things2", remove: "border-dotted" },
+  { time: 95.5, target: ".devices", add: "border-dotted" },
+  { time: 104.2, target: ".things2", remove: "show" },
+  { time: 104.2, target: ".devices", remove: "show" },
+
+  { time: 110.3, target: ".buddhahand", add: "show" },
+  { time: 117, target: ".fellowhumans", add: "show" },
+  { time: 121, target: ".buddhahand", add: "border-dotted" },
+  { time: 123.5, target: ".buddhahand", remove: "border-dotted" },
+  { time: 124, target: ".fellowhumans", add: "border-dotted" },
+  { time: 127, target: ".fellowhumans", remove: "border-dotted" },
+  { time: 126, target: ".buddhahand", remove: "show" },
+  { time: 126, target: ".fellowhumans", remove: "show" },
+
+  { time: 130.5, target: ".rectangles", add: "show" },
+  { time: 143.6, target: ".rectangles", remove: "show" },
+
+  { time: 154.1, target: ".mannequins", add: "show" },
+  { time: 158.8, target: ".fellowhumans", add: "show" },
+  { time: 166, target: ".mannequins", remove: "show" },
+  { time: 166, target: ".fellowhumans", remove: "show" },
+
+  { time: 223.2, target: ".proposal-fullscreen", add: "show" },
+  { time: 224, target: ".proposal-fullscreen", remove: "show" },
 
   // idk
   { time: 9, target: ".changeme2", add: "show" },
